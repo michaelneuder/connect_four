1d056d7577ac6d78a0cf3c44c2065a3d3b44d021
Michael Neuder <michael.neuder@gmail.com>
2017-03-12 18:50:01 -0600

    gdang it

--- connect_four.py
+++ connect_four.py
@@ -10,10 +10,10 @@ class connect_four(object):
 
     def print_board(self):
         print("    "+"-"*17)
-        for x in range(self.size):
-            print("    | {} {} {} {} {} {} {} |".format(self.board[x][0], self.board[x][1],
-            self.board[x][2], self.board[x][3], self.board[x][4], self.board[x][5],
-            self.board[x][6]))
+        for x in range(self.height):
+            print("    | {} {} {} {} {} {} {} |".format(self.board[0][x],
+                self.board[1][x], self.board[2][x], self.board[3][x], self.board[4][x],
+                self.board[5][x], self.board[6][x]))
         print("    "+"-"*17)
 
     def make_move(self, color, column):
@@ -21,77 +21,73 @@ class connect_four(object):
         row = 0
         while(self.board[row][column] == 0 and row < 6):
             row+=1
-        if (row == 0 and self.board[row][column] != 0): # condition for a full column
+        if (row == 0 and self.board[column][row] != 0): # condition for a full column
             print("this column is full, please choose another!")
         else: # not full column
-            if (row == 6 and self.board[row][column] == 0): # case of the first piece in the column
+            if (column == 6 and self.board[column][row] == 0): # case of the first piece in the column
                 if(color == 'red'):
-                    self.board[row][column] = 1
+                    self.board[column][row] = 1
                 elif(color == 'black'):
-                    self.board[row][column] = 2
+                    self.board[column][row] = 2
                 else:
                     print("color error")
             else: # not the first piece of the column
                 print("entered else")
                 if(color == 'red'):
-                    self.board[row-1][column] = 1
+                    self.board[column][row-1] = 1
                 elif(color == 'black'):
-                    self.board[row-1][column] = 2
+                    self.board[column][row-1] = 2
                 else:
                     print("color error")
         print("this is the new board state")
         self.print_board()
 
-
-    def check_win_diag(self):
-        row = 3
-        column = 0
-        for i in range(1)
-            if(self.board[row-i][column+i] == self.board[row-1-i][column+1+i] == self.board[row-2-i][column+2+i]
-            == self.board[row-3-i][column+3+i] != 0): # left diag 1
-                print("we have a winner")
-                return True
-        row = 4
-        column = 0
-        for i in range(2): #left diag 2
-            if(self.board[row-i][column+i] == self.board[row-1-i][column+1+i] == self.board[row-2-i][column+2+i]
-            == self.board[row-3-i][column+3+i] != 0): # left diag 1
-                print("we have a winner")
-                return True
-        row = 5
-        column = 0
-        for i in range(3): #left diag 2
-            if(self.board[row-i][column+i] == self.board[row-1-i][column+1+i] == self.board[row-2-i][column+2+i]
-            == self.board[row-3-i][column+3+i] != 0): # left diag 1
-                print("we have a winner")
-                return True
-
-
-
-
+    #
+    # def check_win_diag(self):
+    #     row = 3
+    #     column = 0
+    #     for i in range(1):
+    #         if(self.board[row-i][column+i] == self.board[row-1-i][column+1+i] == self.board[row-2-i][column+2+i]
+    #         == self.board[row-3-i][column+3+i] != 0): # left diag 1
+    #             print("we have a winner")
+    #             return True
+    #     row = 4
+    #     column = 0
+    #     for i in range(2): #left diag 2
+    #         if(self.board[row-i][column+i] == self.board[row-1-i][column+1+i] == self.board[row-2-i][column+2+i]
+    #         == self.board[row-3-i][column+3+i] != 0): # left diag 1
+    #             print("we have a winner")
+    #             return True
+    #     row = 5
+    #     column = 0
+    #     for i in range(3): #left diag 2
+    #         if(self.board[row-i][column+i] == self.board[row-1-i][column+1+i] == self.board[row-2-i][column+2+i]
+    #         == self.board[row-3-i][column+3+i] != 0): # left diag 1
+    #             print("we have a winner")
+    #             return True
 
 
     def check_win_rc(self):
-        for row in range(self.size): # checking for win along each row
-            for column in range(self.size - 3):
-                if(self.board[row][column] == self.board[row][column+1] == self.board[row][column+2]
-                == self.board[row][column+3] != 0):
-                    if(self.board[row][column] == 1):
+        for row in range(self.height): # checking for win along each row
+            for column in range(self.width - 3):
+                if(self.board[column][row] == self.board[column+1][row] == self.board[column+2][row]
+                == self.board[column+3][row] != 0):
+                    if(self.board[column][row] == 1):
                         print("red wins! four are connected on row", row + 1)
                         return True
-                    elif(self.board[row][column] == 2):
+                    elif(self.board[column][row] == 2):
                         print("black wins! four are connected on row", row + 1)
                         return True
                     else:
                         print("error 1")
-        for column in range(self.size): # checking for win along each column
-            for row in range(self.size - 3):
-                if(self.board[row][column] == self.board[row+1][column] == self.board[row+2][column]
-                == self.board[row+3][column] != 0):
-                    if(self.board[row][column] == 1):
+        for column in range(self.width): # checking for win along each column
+            for row in range(self.height - 3):
+                if(self.board[column][row] == self.board[column][row+1] == self.board[column][row+2]
+                == self.board[column][row+3] != 0):
+                    if(self.board[column][row] == 1):
                         print("red wins! four are connected on column", column + 1)
                         return True
-                    elif(self.board[row][column] == 2):
+                    elif(self.board[column][row] == 2):
                         print("black wins! four are connected on row", column + 1)
                         return True
                     else:
@@ -118,7 +114,7 @@ def main():
             else:
                 cf_board.make_move('black', column_int - 1)
                 cf_board.move_number += 1
-            if(cf_board.check_win_rc() or cf_board.check_win_diag()):
+            if(cf_board.check_win_rc()):# or cf_board.check_win_diag()):
                 quit()
 
 
---
+++ multi_array_practice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+height = 6
+width = 7
+m_dim_list = [[0 for x in range(width)] for y in range(height)]
+
+for x in range(height):
+    print(m_dim_list[x][0], m_dim_list[x][1], m_dim_list[x][2], m_dim_list[x][3],
+        m_dim_list[x][4], m_dim_list[x][5], m_dim_list[x][6])
